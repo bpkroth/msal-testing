828f33d06c00ab155ca5a1fe47c3ba3b7d9f8f30
Brian Kroth <bpkroth@microsoft.com>
2025-09-04 23:43:05 +0000

    script fixups

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # MSAL Testing
 
+Simple repo for hacking around with [MSAL](https://learn.microsoft.com/en-us/entra/identity-platform/msal-overview) and some of its issues with [Secret Service](https://specifications.freedesktop.org/secret-service-spec/latest/) and [Git Credential Manager](https://github.com/git-ecosystem/git-credential-manager) on Linux.
+
+## Usage
+
 ```sh
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+./msal_testing.py
 ```
+
+## See Also
+
+- <https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/issues/3033>
+- <https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/issues/3814#issuecomment-1322634854>
+- <https://github.com/MicrosoftDocs/microsoft-authentication-library-for-python/blob/main/msal-python-conceptual/advanced/linux-broker-py-wsl.md>
--- msal_testing.py
+++ msal_testing.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import base64
+from datetime import datetime, timedelta
 import json
 import time
 import secretstorage
@@ -9,10 +11,23 @@ CLIENT_ID = "872cd9fa-d31f-45e0-9eab-6e460a02d1f1"  # usually GCM has a known cl
 AUTHORITY = "https://login.microsoftonline.com/common"
 SCOPE = ["499b84ac-1321-427f-aa17-267ca6975798/.default"]  # ADO default scope
 
-def get_secret(service, account=None):
+def get_secret_collection():
     bus = secretstorage.dbus_init()
     collection = secretstorage.get_default_collection(bus)
-    items = collection.search_items({"service": service})
+    for item in collection.get_all_items():
+        print(f"Item: {item.get_label()}, Attributes: {item.get_attributes()}")
+    return collection
+
+def get_item(k, v, account=None):
+    collection = get_secret_collection()
+    items = collection.search_items({k: v})
+    for item in items:
+        if account is None or item.get_attributes().get("account") == account:
+            return item.get_secret().decode()
+    return None
+
+def get_secret(service, account=None):
+    items = get_item("service", service, account)
     for item in items:
         if account is None or item.get_attributes().get("account") == account:
             return item.get_secret().decode()
@@ -20,24 +35,41 @@ def get_secret(service, account=None):
 
 def main():
     # 1. Try to get RefreshToken from SecretService
-    refresh_token = get_secret("git:https://dev.azure.com/", None)
+    #secret = get_secret("git:https://dev.azure.com/", None)
+    secret_b64 = get_item("MsalClientID", "Microsoft.Developer.IdentityService")
+    if secret_b64 is None:
+        print("No matching Secret found in SecretService.")
+        return
+    secret_json = base64.b64decode(secret_b64).decode('utf-8')
+    secret = json.loads(secret_json)
+    print(f"Found secret in SecretService: {json.dumps(secret, indent=2)}\n")
+    refresh_token = secret.get("RefreshToken")
     if refresh_token is None:
-        print("No RefreshToken found in SecretService.")
+        print("No RefreshToken found in the secret.")
         return
 
-    print("Found RefreshToken in SecretService.")
+    # Pick the first instance that has a secret
+    refresh_token_secret = None
+    for k,v in refresh_token.items():
+        if v.get("secret") is not None:
+            refresh_token = v
+            refresh_token_secret = v["secret"]
+            break
+    print(f"Found RefreshToken in SecretService: {json.dumps(refresh_token, indent=2)}\n")
 
     # 2. Initialize MSAL PublicClientApplication
     app = PublicClientApplication(client_id=CLIENT_ID, authority=AUTHORITY)
 
     # 3. Try to acquire token silently using the refresh token
     token_response = app.acquire_token_by_refresh_token(
-        refresh_token, scopes=SCOPE
+        refresh_token_secret, scopes=SCOPE,
     )
 
+    print()
+
     if "access_token" in token_response:
-        print("Successfully acquired new AccessToken!")
-        print("Expires at:", time.ctime(token_response["expires_on"]))
+        print(f"Successfully acquired new AccessToken! {json.dumps(token_response, indent=2)}\n")
+        print("Expires at:", datetime.now() + timedelta(seconds=token_response["expires_in"]))
     else:
         print("Failed to acquire AccessToken via RefreshToken.")
         print(json.dumps(token_response, indent=2))
